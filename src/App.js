9748aab8e47fc1afbf5e47182d59119a0cb77dc3
Julio Cesar Zapata <ingeniero.desarrollo5@comersantander.com>
2021-05-25 19:49:12 -0500

    Cambios

--- src/App.js
+++ src/App.js
@@ -42,7 +42,6 @@ class App extends Component {
     const { currentUser, showClienteBoard, showAdminBoard } = this.state;
 
     return (
-      
       <div>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous"></link>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
